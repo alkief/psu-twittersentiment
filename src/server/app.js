b6479353a0ff7885eac672a6d307fed8be52e958
Matthew Benkart <mthwate@mthwate.com>
2017-11-26 21:39:50 -0500

    Added an /api/sentiment example endpoint

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,7 @@
 'use strict'
 require('babel-core').transform('code')
 import "babel-polyfill"
+import _ from 'lodash'
 
 // Load variables from '.env' in the project root
 import dotenv from 'dotenv'
@@ -11,7 +12,9 @@ import path from 'path' // Resolves relative pathing issues
 import bodyParser from 'body-parser' // Utility for parsing request bodies
 import fs from 'fs' // Allows file system I/O
 
-import Controller from './scripts/Controller' // The main application controller
+import Controller from './scripts/Controller'
+import TwitterMessenger from "./scripts/TwitterMessenger";
+import WatsonMessenger from "./scripts/WatsonMessenger"; // The main application controller
 
 
 // Instantiate the application controller
@@ -19,6 +22,8 @@ import Controller from './scripts/Controller' // The main application controller
 // 		creation of this object
 let controller = new Controller()
 
+let twitter = new TwitterMessenger()
+let watson = new WatsonMessenger()
 
 // Define the express app
 const app = express()
@@ -26,7 +31,7 @@ const app = express()
 // Look at this directory for '/' paths e.g. localhost:8080/index.html
 app.use(express.static(path.join(__dirname, '../client/static'))) 
 // Look at this directory for '/css' paths
-app.use('/css', express.static(path.join(__dirname, '../client/static'))) 
+app.use('/css', express.static(path.join(__dirname, '../client/static')))
 // Look at this directory for '/js' paths
 app.use('/js', express.static(path.join(__dirname, '../client/js'))) 
 
@@ -35,8 +40,45 @@ app.use(bodyParser.json()) // Parse request payloads as JSON
 
 // Respond with JSON representing server-side analysis of the most recent set of tweets
 app.get('/data/batch', (req, res) => {
-	res.send('response')
-	res.set("Connection", "close")
+    res.send('response')
+    res.set("Connection", "close")
+})
+
+app.get('/api/sentiment', (req, res) => {
+    twitter.getTweets(['PSU', 'Penn State', 'Penn State University'], 500).then((tweets) => {
+
+        let tweetText = _.map(tweets, 'text').join('\n')
+
+        watson.nluAnalyzeText(tweetText).then((ret) => {
+
+            function transform(data) {
+                return {
+                    text: data.text,
+                    sentiment: data.sentiment.score,
+                    sadness: data.emotion.sadness,
+                    joy: data.emotion.joy,
+                    fear: data.emotion.fear,
+                    disgust: data.emotion.disgust,
+                    anger: data.emotion.anger
+                }
+            }
+
+            let keywords = _.filter(ret.keywords, (o) => {
+                return o.emotion && o.relevance >= 0.5
+            })
+
+            let entities = _.filter(ret.entities, (o) => {
+                return o.emotion && o.relevance >= 0.5
+            })
+
+            keywords = _.map(keywords, transform)
+            entities = _.map(entities, transform)
+
+            let result = _.concat(keywords, entities)
+
+            res.send(JSON.stringify(result))
+        })
+    })
 })
 
 let port = process.env.PORT || process.env.VCAP_APP_PORT || 8080
